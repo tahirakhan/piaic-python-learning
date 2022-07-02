b12f4aab2e86e7aae8f359d7ce953a5af1ea840e
Tahir <>
2022-07-02 12:20:04 +0500

    output of assignment

--- Day3-2-07-22/example10.py
+++ Day3-2-07-22/example10.py
@@ -21,3 +21,21 @@ totalMarks = engMarks + urduMarks + mathsMarks + sciMarks + artsMarks
 
 print("Total Marks", '*****************', (totalMarks))
 print("Average Marks", '***************', totalMarks*100/(total * 5) + "%")
+
+
+# Enter Student Name: Tahir
+# Enter total marks of each subject: 100
+# Enter marks in english: 67
+# Enter marks in urdu: 87
+# Enter marks in maths: 76
+# Enter marks in science: 55
+# Enter marks in arts: 67
+# **********MARKS SHEET************
+# Student Name :  Tahir
+# English ********************* 67
+# Urdu ************************ 87
+# Maths *********************** 76
+# Science ********************* 55
+# Arts ************************ 67
+# Total Marks ***************** 352
+# Average Marks *************** 70.4
